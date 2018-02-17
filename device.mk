a7a4756a2d6bbb951ef24a5f0341ca8307c8a18f
deadman96385 <seanhoyt963@gmail.com>
2018-02-17 17:42:49 -0600

    Import AOSP Bluetooth binderized hal

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-service.8gb
 
 # Sensors
 PRODUCT_PACKAGES += \
